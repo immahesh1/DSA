bddd9608aa427ffaee9f69a864407db16055398b
Mahesh Kumar <mahesh_kumar1019@yahoo.in>
2020-09-18 16:55:16 +0530

    Numeric pattern added

---
+++ Basic of Programming/Patterns/Pattern_14.cpp
@@ -0,0 +1,26 @@
+// 3*1=3
+// 3*2=6
+// 3*3=9
+// 3*4=12
+// 3*5=15
+// 3*6=18
+// 3*7=21
+// 3*8=24
+// 3*9=27
+// 3*10=30
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= 10; i++) {
+		cout << n << " * " << i << " = " << n*i << endl;
+	}
+
+
+	return 0;
+}
