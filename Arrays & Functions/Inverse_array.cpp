19915ba7d292c3f671bb9215d3ca44b3cd86e4f9
Mahesh Kumar <mahesh_kumar1019@yahoo.in>
2020-09-23 20:01:02 +0530

    Array prog add

---
+++ Arrays & Functions/Inverse_array.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int ar[n];
+	for (int i = 0; i < n; i++) {
+		cin >> ar[i];
+	}
+
+	int iar[n];
+
+	for (int i = 0; i < n; i++) {
+		iar[ar[i]] = i;
+	}
+
+	for (int a : iar) {
+		cout << a << endl;
+	}
+	return 0;
+}
