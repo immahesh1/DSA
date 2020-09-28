28d3e1d71f860d8fdbec4692127024388b4811cd
Mahesh Kumar <mahesh_kumar1019@yahoo.in>
2020-09-28 13:53:47 +0530

    Recursion prog add

---
+++ Recursion & Backtracking/Intro to Recursion/print_increasing.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print_increasing(int n) {
+	if (n == 0) {
+		return;
+	}
+	print_increasing(n - 1);
+	cout << n << endl;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	print_increasing(n);
+
+	return 0;
+}
