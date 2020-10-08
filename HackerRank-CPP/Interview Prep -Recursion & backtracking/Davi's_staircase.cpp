9a3af2d35b807128b98b49d46ac87e1cfd6c66a1
Mahesh Kumar <30316795+immahesh1@users.noreply.github.com>
2020-10-08 15:39:02 +0530

    Update Davi's_staircase.cpp

--- HackerRank-CPP/Interview Prep -Recursion & backtracking/Davi's_staircase.cpp
+++ HackerRank-CPP/Interview Prep -Recursion & backtracking/Davi's_staircase.cpp
@@ -17,12 +17,6 @@ int count_staircase_ways(int n) {
 
 int main()
 {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#ifndef ONLINE_JUDGE
-	freopen("input1.txt", "r", stdin);
-	freopen("output1.txt", "w", stdout);
-#endif
-
 	int t;
 	cin >> t;
 	while (t-- > 0) {
